9f41cdfdff0c35451bea025033dfb8b1d1fa2724
Arjan <github@avbentem.dds.nl>
2019-03-21 19:13:25 +0100

    Updates

---
+++ precache-manifest.200dd87ebb5e45afdbea174390f9afe7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "84bd4ce837e8bfac810d",
+    "url": "/lorawan-airtime-ui/static/js/runtime~main.2e301313.js"
+  },
+  {
+    "revision": "32b04964568761373eb1",
+    "url": "/lorawan-airtime-ui/static/js/main.ffcee726.chunk.js"
+  },
+  {
+    "revision": "395a4d702b7058a44d33",
+    "url": "/lorawan-airtime-ui/static/js/2.ac79f000.chunk.js"
+  },
+  {
+    "revision": "32b04964568761373eb1",
+    "url": "/lorawan-airtime-ui/static/css/main.e9e9b3b4.chunk.css"
+  },
+  {
+    "revision": "035a74d090dbdc69e6c7a43775545c90",
+    "url": "/lorawan-airtime-ui/index.html"
+  }
+];
