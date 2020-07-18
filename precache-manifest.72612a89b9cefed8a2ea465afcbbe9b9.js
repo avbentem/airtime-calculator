3274dffb1358d6926b5341f285f888949b86e926
Arjan <github@avbentem.dds.nl>
2020-07-18 17:21:00 +0200

    Updates

---
+++ precache-manifest.72612a89b9cefed8a2ea465afcbbe9b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9042500c3a3152bf28627eea1f7ae261",
+    "url": "/lorawan-airtime-ui/index.html"
+  },
+  {
+    "revision": "78c224d417853650c9d5",
+    "url": "/lorawan-airtime-ui/static/css/main.bf7a00e2.chunk.css"
+  },
+  {
+    "revision": "b1aa182ce3f632f9aba4",
+    "url": "/lorawan-airtime-ui/static/js/2.0a0f1414.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/lorawan-airtime-ui/static/js/2.0a0f1414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78c224d417853650c9d5",
+    "url": "/lorawan-airtime-ui/static/js/main.ff0f75c5.chunk.js"
+  },
+  {
+    "revision": "98f7388998bc708064bd",
+    "url": "/lorawan-airtime-ui/static/js/runtime-main.b2d30af9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lorawan-airtime-ui/precache-manifest.a29234d1d4559d5f604469bc3345c8c9.js"
+  "/lorawan-airtime-ui/precache-manifest.72612a89b9cefed8a2ea465afcbbe9b9.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/lorawan-airtime-ui/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lorawan-airtime-ui/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
