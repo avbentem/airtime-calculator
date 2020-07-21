91067e9167b5c69ebfa6daa60050bbda137b26cb
Arjan <github@avbentem.dds.nl>
2020-07-21 11:43:10 +0200

    Updates

--- precache-manifest.66a1e12d9f135c572bde05a5e32e19c6.js
+++ precache-manifest.66a1e12d9f135c572bde05a5e32e19c6.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3f5d5aaf36ba9fa5e07cdd391568359a",
+    "revision": "392bb6f3a20f987c05fca28cfc76d4f3",
     "url": "/airtime-calculator/index.html"
   },
   {
-    "revision": "d20fff93f40ae60dafa0",
-    "url": "/airtime-calculator/static/css/main.cfe2e4c2.chunk.css"
+    "revision": "42680b8fe68ef934f8b8",
+    "url": "/airtime-calculator/static/css/main.46788942.chunk.css"
   },
   {
     "revision": "42bb8717c7584a653b45",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/airtime-calculator/static/js/2.3a47387f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d20fff93f40ae60dafa0",
+    "revision": "42680b8fe68ef934f8b8",
     "url": "/airtime-calculator/static/js/main.0bed6892.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/airtime-calculator/precache-manifest.2847c4698f088ee0df30e102368ac254.js"
+  "/airtime-calculator/precache-manifest.66a1e12d9f135c572bde05a5e32e19c6.js"
 );
 
 self.addEventListener('message', (event) => {
